378e7080bd3baa2fc121857d2d3664f76e69aa6e
Christian Meter <cmeter@googlemail.com>
2020-12-02 12:18:05 +0100

    Solve day1

---
+++ src/advent/day1.clj
@@ -0,0 +1,16 @@
+(ns advent.day1
+  (:require [advent.lib :as lib]))
+
+(def sample [1721 979 366 299 675 1456])
+(def input (lib/read-multiline-file "day1"))
+
+(defn find-product [l]
+  (first
+    (for [x l
+          y l
+          z l
+          :when (= 2020 (+ x y z))]
+      (* x y z))))
+
+(time
+  (find-product input))
---
+++ src/advent/lib.clj
@@ -0,0 +1,12 @@
+(ns advent.lib
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn read-multiline-file [file]
+  (->> file
+       io/resource
+       slurp
+       string/split-lines
+       (map read-string)))
+
+(read-multiline-file "day1")
